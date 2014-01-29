b750dfed844602ac8d8c82a5f76a0e973ed1d39a
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-01-29 00:32:49 -0300

    Refactor some lines in todo controller and template

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -2,19 +2,11 @@
 
 angular.module('singularPracticeApp')
   .controller('TodoCtrl', ['$scope', 'TodoService', function ($scope, todoService) {
-    $scope.todos = todoService;
-
-    $scope.todos.$bind($scope, 'todos');
+    todoService.$bind($scope, 'todos');
 
     $scope.addTodo = function () {
       $scope.todos.$add({text: $scope.todoText, done:false});
       $scope.todoText = '';
-    };
-
-    $scope.toggle = function (id) {
-      var todo = $scope.todos[id];
-      todo.done = !todo.done;
-      $scope.todos.$save(id);
     };
 
     $scope.remaining = function () {
